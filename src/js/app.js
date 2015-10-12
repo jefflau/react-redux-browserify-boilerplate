2d9bc3b5c4619709833590e8c027fb5886b87127
Jeff Lau <jeff-lau@live.com>
2015-10-12 19:33:01 +0800

    Removed example Promise

--- src/js/app.js
+++ src/js/app.js
@@ -5,6 +5,19 @@ import Radium from 'radium';
 
 import { Nav } from './components/nav';
 
+var someAsyncThing = function() {
+  return new Promise(function(resolve, reject) {
+    // this will throw, x does not exist
+    resolve(x + 2);
+  });
+};
+
+someAsyncThing().then(function() {
+  console.log('everything is great');
+}).catch(function(error) {
+  console.log('oh no', error);
+});
+
 @Radium
 class App extends React.Component {
   render() {
